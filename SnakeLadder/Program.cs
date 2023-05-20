d1421419673d520cba42dd85c8a0e767660729d9
“karandepale” <karandepale111@gmail.com>
2023-05-20 09:45:26 -0700

    Add: Repeat till the Player reaches the winning Position 100

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -11,29 +11,37 @@ namespace SnakeLadder
             Console.WriteLine("Start Position is: " + currentPosition);
             Random random = new Random();
 
-            // UC2:- Roll the die to get a random number between 1 and 6
-            int dieRoll = random.Next(1, 7);
-            Console.WriteLine("The player rolled a die and got: " + dieRoll);
-
-            // UC3:- Check for options: No Play, Ladder, or Snake
-            int option = random.Next(0, 3); // Randomly select an option (0 = No Play, 1 = Ladder, 2 = Snake)
-
-            switch (option)
+            while (currentPosition < finalPosition)
             {
-                case 0: // No Play
-                    Console.WriteLine("No Play. The player stays in the same position.");
-                    break;
-                case 1: // Ladder
-                    Console.WriteLine("Ladder. The player moves ahead by " + dieRoll + " positions.");
-                    currentPosition += dieRoll;
-                    break;
-                case 2: // Snake
-                    Console.WriteLine("Snake. The player moves behind by " + dieRoll + " positions.");
-                    currentPosition -= dieRoll;
-                    break;
+                // UC2:- Roll the die to get a random number between 1 and 6
+                int dieRoll = random.Next(1, 7);
+                Console.WriteLine("The player rolled a die and got: " + dieRoll);
+
+                // UC3:- Check for options: No Play, Ladder, or Snake
+                int option = random.Next(0, 3); // Randomly select an option (0 = No Play, 1 = Ladder, 2 = Snake)
+
+                switch (option)
+                {
+                    case 0: // No Play
+                        Console.WriteLine("No Play. The player stays in the same position.");
+                        break;
+                    case 1: // Ladder
+                        Console.WriteLine("Ladder. The player moves ahead by " + dieRoll + " positions.");
+                        currentPosition += dieRoll;
+                        break;
+                    case 2: // Snake
+                        Console.WriteLine("Snake. The player moves behind by " + dieRoll + " positions.");
+                        currentPosition -= dieRoll;
+
+                        if (currentPosition < 0)
+                            currentPosition = 0; // Restart from position 0 if position goes below 0
+                        break;
+                }
+
+                Console.WriteLine("Current Position: " + currentPosition);
             }
 
-            Console.WriteLine("Current Position: " + currentPosition);
+            Console.WriteLine("Congratulations! The player has reached the winning position.");
         }
     }
 }
