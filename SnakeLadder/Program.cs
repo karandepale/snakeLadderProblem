7a32fe250903767caca41228ce229a17bcfaa32d
“karandepale” <karandepale111@gmail.com>
2023-05-20 09:09:17 -0700

    Welcome to sname ladder program

---
+++ SnakeLadder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake Ladder Program...");
+        }
+    }
+}
