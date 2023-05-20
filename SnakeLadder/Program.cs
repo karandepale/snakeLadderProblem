9be55c995bb32386b411e3e4eaecbf2a4aea471f
“karandepale” <karandepale111@gmail.com>
2023-05-20 09:40:47 -0700

    Add:The Player then checks for a Option. They are No Play,Ladder or Snake

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -11,9 +11,29 @@ namespace SnakeLadder
             Console.WriteLine("Start Position is: " + currentPosition);
             Random random = new Random();
 
-            // UC2:-  Roll the die to get a random number between 1 and 6
+            // UC2:- Roll the die to get a random number between 1 and 6
             int dieRoll = random.Next(1, 7);
             Console.WriteLine("The player rolled a die and got: " + dieRoll);
+
+            // UC3:- Check for options: No Play, Ladder, or Snake
+            int option = random.Next(0, 3); // Randomly select an option (0 = No Play, 1 = Ladder, 2 = Snake)
+
+            switch (option)
+            {
+                case 0: // No Play
+                    Console.WriteLine("No Play. The player stays in the same position.");
+                    break;
+                case 1: // Ladder
+                    Console.WriteLine("Ladder. The player moves ahead by " + dieRoll + " positions.");
+                    currentPosition += dieRoll;
+                    break;
+                case 2: // Snake
+                    Console.WriteLine("Snake. The player moves behind by " + dieRoll + " positions.");
+                    currentPosition -= dieRoll;
+                    break;
+            }
+
+            Console.WriteLine("Current Position: " + currentPosition);
         }
     }
 }
